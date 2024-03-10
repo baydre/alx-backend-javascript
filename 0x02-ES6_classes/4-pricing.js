4cc25e3752991d4cfcaa5af65dd39c877ab2d5d5
baydre_africa <baydreafrica@gmail.com>
2024-03-10 17:48:56 +0100

    static method implementation in ES6

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+/* eslint-disable */
+import Currency from './3-currency.js';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('amount must be a number');
+    }
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(newAmount) {
+    if (typeof newAmount !== 'number') throw new TypeError('amount must be a number');
+    this._amount = newAmount;
+  }
+
+  get currency() {
+    return this._amount;
+  }
+
+  set currency(newCurrency) {
+    if (!(newCurrency instanceof Currency)) throw new TypeError('currency must be an instance of Currency');
+    this._currency = newCurrency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency.name} (${this._currency.code})`;
+  }
+
+  // static method implementation
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') throw new TypeError('amount must be a number');
+    if (typeof conversionRate !== 'number') throw new TypeError('conversionRate must be a number');
+    return amount * conversionRate;
+  }
+}
