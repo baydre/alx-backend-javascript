b9ee99827c1535d6e11ee32f3bd759b76595b39a
baydre_africa <baydreafrica@gmail.com>
2024-03-12 21:29:30 +0100

    Update: function named createInt8TypedArray that returns new ArrayBuffer

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -2,9 +2,10 @@ export default function createInt8TypedArray(length, position, value) {
   if (position < 0 || position >= length) {
     throw new Error('Position outside range');
   }
-  const buffer = new DataView(new ArrayBuffer(length), 0, length);
+  const buffer = new ArrayBuffer(length);
+  const dataView = new DataView(buffer);
   const int8View = new Int8Array(buffer);
   int8View[position] = value;
 
-  return buffer;
+  return dataView;
 }
