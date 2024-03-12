11c6bb18ae7833f37f05dd806e7c0befc2f1c26d
baydre_africa <baydreafrica@gmail.com>
2024-03-12 09:11:15 +0100

    function that getStudentsIdList using the map()

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,6 +1,5 @@
 export default function getListStudentsIds(arrOfObjects) {
   // if args ![..], return []
-  // const getId = id.map(getListStudentsIds);
   if (Array.isArray(arrOfObjects)) {
     const getId = arrOfObjects.map((item) => item.id);
     return getId;
